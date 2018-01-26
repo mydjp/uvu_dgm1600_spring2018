844488acec7c213556f8aab86a3721cb4ac587c6
Unknown <mydjp0912@yahoo.com>
2018-01-25 19:25:17 -0700

    make a moving box

--- RobotTD/Assets/Scripts/CubeMove.cs
+++ RobotTD/Assets/Scripts/CubeMove.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour {
 
-	public float transX;
-	public float transY;
+	// public float transX;
+	// public float transY;
+	// public float transZ; 
+	
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
 
-	public float transZ; 
-
-	public float rotX;
-	public float rotY;
-
-	public float rotZ; 
+		// public float moveSpeed;
+		// public float turnSpeed;
+		// public float jumpHeight;
+		public float moveSpeed;
 
 
 	// Use this for initialization
@@ -22,7 +25,23 @@ public class CubeMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(transX,transY,transZ);
-		transform.Rotate(rotX,rotY,rotZ);
+		// transform.Translate(transX,transY,transZ);
+		// transform.Rotate(rotX,rotY,rotZ);
+		// var j = Input.GetAxis("jump")* Time.deltaTime * jumpHeight;
+		// var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		// var z = Input.GetAxis("")* Time.deltaTime * turnSpeed;
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.Q)){
+			transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+		}
+			if(Input.GetKey(KeyCode.E)){
+			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+		}
 	}
 }
---
+++ RobotTD/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	
+
+		 public float moveSpeed;
+		 public float turnSpeed;
+		 public float jumpHeight;
+		
+	
+	
+	// Update is called once per frame
+	void Update () {
+				// transform.Translate(transX,transY,transZ);
+		// transform.Rotate(rotX,rotY,rotZ);
+		 var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		 var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnSpeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * turnSpeed;
+		
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+	}
+}
