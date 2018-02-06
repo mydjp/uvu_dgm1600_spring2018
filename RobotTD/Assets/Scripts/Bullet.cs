6ad96893bd980c3087b1054d076e39a0bb4ff14d
Unknown <mydjp0912@yahoo.com>
2018-02-06 14:30:29 -0700

    upadated stuff

---
+++ RobotTD/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public int damage = 1;
+
+	public int time = 5;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyBullet());
+		
+	}
+
+	
+	
+	// Update is called once per frame
+	
+	IEnumerator DestroyBullet(){
+		yield return new WaitForSeconds(time);
+		Destroy(gameObject);
+	}
+
+}
---
+++ RobotTD/Assets/Scripts/shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+	public Rigidbody projecticle;
+	public Transform shootPoint;
+	public int shootSpeed;
+
+	// Use this for initialization
+	
+		
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			
+			Rigidbody clone;
+			
+			clone = (Rigidbody)Instantiate(projecticle, shootPoint.position, projecticle.rotation);
+
+			clone.velocity = shootPoint.TransformDirection (Vector3.forward*shootSpeed*Time.deltaTime);
+		}
+		
+	}
+}
